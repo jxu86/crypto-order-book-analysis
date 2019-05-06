7aa3cad6e979eab98566c8579c8483759c635135
JC86 <jxu1@kalengo.com>
2019-05-06 11:38:33 +0800

    update

--- net_grid_strategy.py
+++ net_grid_strategy.py
@@ -55,7 +55,7 @@ class Strategy():
         self.quote_init_amount = 0
         self.grid_num = params.grid_num
         self.pending_orders = self.get_pending_orders(self.spot_pair)
-        self._mongodb = MongoService(host=config.mongo_host, port=config.mongo_port, username=config.mongo_username, password=config.mongo_password)
+        self._mongodb = MongoService(host=config.mongo_host, port=config.mongo_port, username=config.mongo_username, password=config.mongo_password, authSource='nowdone')
 
         print('grid_list=>', self.grid_list)
         print('grid_index=>', self.grid_index)
